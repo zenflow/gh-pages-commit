20db27c9e39931fc4841d3a2845b60f1a7b313b5
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-05-22 03:59:16 -0400

    empty build dir in test; added turtle to test; fixed git add command

--- tests/index.js
+++ tests/index.js
@@ -6,12 +6,14 @@ var fs_extra = require('fs-extra');
 
 var html_in = fs.readFileSync(path.join(__dirname, 'src/index.html'), 'utf8');
 var html_out = html_in.replace(/~uuid~/, uuid.v1());
-fs_extra.ensureDirSync(path.join(__dirname, 'build'));
+fs_extra.emptyDirSync(path.join(__dirname, 'build'));
 fs.writeFileSync(path.join(__dirname, 'build/index.html'), html_out);
+fs_extra.copySync(path.join(__dirname, 'src', 'images'), path.join(__dirname, 'build', 'images'));
+
 ghPagesCommit('tests/build', {
 	basedir: path.join(__dirname, '..'),
 	verbose: true
 }, function(error){
 	if (error){throw error;}
-	console.log('done');
+	console.log('Success!');
 });
